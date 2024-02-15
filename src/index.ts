6bbe1900df76a42b67bc931d00da63a278de8a79
James Wu <88938117+thejwuscript@users.noreply.github.com>
2024-02-15 23:56:13 +0900

    Install socket.io

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,20 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import { Server } from 'socket.io';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+  console.log("listening on port:" + port)
+})
+const io = new Server(server)
 
 app.get("/", (req: Request, res: Response) => {
   res.send("My Server");
 });
 
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
-});
+io.on('connection', (socket) => {
+  console.log("a user connected")
+})
