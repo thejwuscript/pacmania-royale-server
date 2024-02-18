cfcc57fe8e7a17db4fc4b2565bdf6a3115c7afa7
James Wu <88938117+thejwuscript@users.noreply.github.com>
2024-02-18 22:11:46 +0900

    Hold off using sqlite

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import { existsSync, unlinkSync } from 'fs';
+// import { existsSync, unlinkSync } from 'fs';
 import express from "express";
 import dotenv from "dotenv";
 import { Server } from 'socket.io';
-import sqlite3 from "sqlite3";
-import { open } from 'sqlite';
+// import sqlite3 from "sqlite3";
+// import { open } from 'sqlite';
 import { uniqueNamesGenerator, adjectives, animals } from 'unique-names-generator';
 
 dotenv.config();
@@ -22,27 +22,29 @@ const io = new Server(server, {
   }
 })
 
-if (process.env.NODE_ENV === "development" && existsSync("database.db")) {
-  try {
-    unlinkSync("database.db");
-    console.log('Database file deleted successfully');
-  } catch (error) {
-    console.error('Error deleting database file:', error);
-  }
+// if (process.env.NODE_ENV === "development" && existsSync("database.db")) {
+//   try {
+//     unlinkSync("database.db");
+//     console.log('Database file deleted successfully');
+//   } catch (error) {
+//     console.error('Error deleting database file:', error);
+//   }
 
-}
+// }
 
-const db = await open({
-  filename: 'database.db',
-  driver: sqlite3.Database
-});
+// const db = await open({
+//   filename: 'database.db',
+//   driver: sqlite3.Database
+// });
 
-await db.exec(`
-  CREATE TABLE IF NOT EXISTS users (
-    id TEXT PRIMARY KEY,
-    name TEXT
-  );
-`)
+// await db.exec(`
+//   CREATE TABLE IF NOT EXISTS users (
+//     id TEXT PRIMARY KEY,
+//     name TEXT
+//   );
+// `)
+
+let connectedUsers = new Map();
 
 io.on('connection', async (socket) => {
   // socket.join("lobby")
@@ -52,19 +54,24 @@ io.on('connection', async (socket) => {
     length: 2
   });
 
-  await db.exec(`INSERT INTO users (id, name) VALUES ('${socket.id}', '${username}')`);
+  // await db.exec(`INSERT INTO users (id, name) VALUES ('${socket.id}', '${username}')`);
+  connectedUsers.set(socket.id, { name: username })
 
   console.log(`User ${username} has connected.`)
 
-  socket.emit('user data', { id: socket.id, name: username })
+  socket.emit('my user data', { id: socket.id, name: username })
   socket.broadcast.emit("user connected", { name: username })
+  io.emit("connected users", Array.from(connectedUsers.values()))
 
   socket.on("disconnect", async () => {
     try {
-      const row = await db.get(`SELECT * FROM users WHERE id = ?`, [socket.id]);
-      console.log(`User ${row.name} has disconnected.`)
-      await db.exec(`DELETE FROM users WHERE id = '${socket.id}'`)
-      io.emit('user disconnected', { name: row.name });
+      // const row = await db.get(`SELECT * FROM users WHERE id = ?`, [socket.id]);
+      const disconnectedUser = connectedUsers.get(socket.id)
+      console.log(`User ${disconnectedUser.name} has disconnected.`)
+      // await db.exec(`DELETE FROM users WHERE id = '${socket.id}'`)
+      connectedUsers.delete(socket.id)
+      io.emit('user disconnected', { name: disconnectedUser.name });
+      io.emit("connected users", Array.from(connectedUsers.values()))
     } catch (error) {
       if (error instanceof Error) {
         console.error('Error:', error.message);
