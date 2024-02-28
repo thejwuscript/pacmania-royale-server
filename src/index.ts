de44887888b844e6d727e0a84e1e72013af71d63
James Wu <88938117+thejwuscript@users.noreply.github.com>
2024-02-28 17:44:34 +0900

    Clean up comments

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
-// import { existsSync, unlinkSync } from 'fs';
 import express from "express";
 import dotenv from "dotenv";
 import { Server } from 'socket.io';
-// import sqlite3 from "sqlite3";
-// import { open } from 'sqlite';
 import { uniqueNamesGenerator, adjectives, animals } from 'unique-names-generator';
 
 dotenv.config();
@@ -22,39 +19,14 @@ const io = new Server(server, {
   }
 })
 
-// if (process.env.NODE_ENV === "development" && existsSync("database.db")) {
-//   try {
-//     unlinkSync("database.db");
-//     console.log('Database file deleted successfully');
-//   } catch (error) {
-//     console.error('Error deleting database file:', error);
-//   }
-
-// }
-
-// const db = await open({
-//   filename: 'database.db',
-//   driver: sqlite3.Database
-// });
-
-// await db.exec(`
-//   CREATE TABLE IF NOT EXISTS users (
-//     id TEXT PRIMARY KEY,
-//     name TEXT
-//   );
-// `)
-
 let connectedUsers = new Map();
 
 io.on('connection', async (socket) => {
-  // socket.join("lobby")
-
   const username = uniqueNamesGenerator({
     dictionaries: [adjectives, animals],
     length: 2
   });
 
-  // await db.exec(`INSERT INTO users (id, name) VALUES ('${socket.id}', '${username}')`);
   connectedUsers.set(socket.id, { name: username })
 
   console.log(`User ${username} has connected.`)
@@ -69,10 +41,8 @@ io.on('connection', async (socket) => {
 
   socket.on("disconnect", async () => {
     try {
-      // const row = await db.get(`SELECT * FROM users WHERE id = ?`, [socket.id]);
       const disconnectedUser = connectedUsers.get(socket.id)
       console.log(`User ${disconnectedUser.name} has disconnected.`)
-      // await db.exec(`DELETE FROM users WHERE id = '${socket.id}'`)
       connectedUsers.delete(socket.id)
       io.emit('disconnected', { name: disconnectedUser.name });
       io.emit("update user list", Array.from(connectedUsers.values()))
@@ -83,6 +53,5 @@ io.on('connection', async (socket) => {
         console.error('Unknown error:', error);
       }
     }
-
   })
 })
