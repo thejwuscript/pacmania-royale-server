2959befdc4d153b578a11279d05066ae3437a37a
James Wu <88938117+thejwuscript@users.noreply.github.com>
2024-03-22 17:31:30 +0900

    Add healthcheck endpoint

--- src/routes.ts
+++ src/routes.ts
@@ -5,5 +5,8 @@ const router = Router();
 
 router.post("/gameroom", GameroomController.createGameroom);
 router.get("/gamerooms", GameroomController.index);
+router.get("/healthcheck", (req, res) => {
+  res.status(200).send("OK");
+});
 
 export default router;
